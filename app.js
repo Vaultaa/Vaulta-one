b16f5fd247e3a44d20d878b4d361e5eb9f4830cc
Vaultaa <marclaframboise75@gmail.com>
2025-09-20 21:56:55 -0400

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("root").innerHTML = `
-  <h1>🚀 Welcome to Vaulta</h1>
-  <p>Your all-in-one financial super app.</p>
-  <button onclick="alert('Vaulta is live!')">Click Me</button>
-`;
